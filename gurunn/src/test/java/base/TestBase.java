8cd0d7fb5d716632832736745745e35f9e1fd279
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2018-12-25 14:08:33 +0100

    TestBase is updated so that it will have base driver object

--- gurunn/src/test/java/base/TestBase.java
+++ gurunn/src/test/java/base/TestBase.java
@@ -3,22 +3,29 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class TestBase {
-	WebDriver dr = null;
+	public static WebDriver dr ;
 
-	@BeforeClass
-	public void initializeTest() {
+	@BeforeMethod
+	public void initializeTest() throws InterruptedException {
 		System.out.println("Open the browser: " + "\n");
 		System.setProperty("webdriver.chrome.driver",
 				System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver\\chromedriver.exe");
-		System.out.println(System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver\\chromedriver.exe");
 		dr = new ChromeDriver();
 		System.out.println("Chrome driver initialized");
+		dr.get("http://live.guru99.com/");
+		Thread.sleep(3000);
+		dr.manage().window().maximize();
+
 	}
 
-	@AfterClass
+	@AfterMethod
 	public void tearDownTest() {
 		dr.close();
 		System.out.println("Driver is closed");

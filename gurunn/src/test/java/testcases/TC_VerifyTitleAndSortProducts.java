b06c025b43d11e2d1bf22ae82cb34e65566f2a1a
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2018-12-25 14:10:30 +0100

    One more test case is added to the test case

--- gurunn/src/test/java/testcases/TC_VerifyTitleAndSortProducts.java
+++ gurunn/src/test/java/testcases/TC_VerifyTitleAndSortProducts.java
@@ -1,12 +1,30 @@
 package testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.TestBase;
+import pageobjects.Guru99HomePage;
 
-public class TC_VerifyTitleAndSortProducts extends TestBase{
-  @Test
-  public void f() {
-	  System.out.println("this is test");
-  }
+public class TC_VerifyTitleAndSortProducts extends TestBase {
+
+	Guru99HomePage guruhome;
+
+	@Test
+	public void verifyTitleAndSortProducts() {
+		guruhome = new Guru99HomePage(dr);
+		System.out.println("this is test");
+		System.out.println(dr.getTitle());
+		Assert.assertTrue(dr.getTitle().equalsIgnoreCase("Home page"),
+				"The page title is not HOME PAGE. Raise a defect");
+		guruhome.clickMobile();
+		System.out.println("verify title is completed");
+	}
+
+	@Test
+	public void verifyTitle() {
+		guruhome = new Guru99HomePage(dr);
+		guruhome.clickTV();
+
+	}
 }

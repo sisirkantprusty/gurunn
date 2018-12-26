eae484a0d60d6a5a23eae4de2a6929048a484249
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2018-12-26 21:37:08 +0100

    Updated verifyTitleAndSortProducts to verify sorting of phone names

--- gurunn/src/test/java/testcases/TC_VerifyTitleAndSortProducts.java
+++ gurunn/src/test/java/testcases/TC_VerifyTitleAndSortProducts.java
@@ -11,17 +11,20 @@ public class TC_VerifyTitleAndSortProducts extends TestBase {
 	Guru99HomePage guruhome;
 
 	@Test
-	public void verifyTitleAndSortProducts() {
+	public void verifyTitleAndSortProducts() throws InterruptedException {
 		guruhome = new Guru99HomePage(dr);
 		System.out.println("this is test");
 		System.out.println(dr.getTitle());
 		Assert.assertTrue(dr.getTitle().equalsIgnoreCase("Home page"),
 				"The page title is not HOME PAGE. Raise a defect");
 		guruhome.clickMobile();
+		Thread.sleep(2000);
+		guruhome.selectValueMobileSorting("Name");
+		Assert.assertTrue(guruhome.sortAndComparePhoneNames(), "The sequence of phone names are not in aphabetical order");
 		System.out.println("verify title is completed");
 	}
 
-	@Test
+	// @Test
 	public void verifyTitle() {
 		guruhome = new Guru99HomePage(dr);
 		guruhome.clickTV();

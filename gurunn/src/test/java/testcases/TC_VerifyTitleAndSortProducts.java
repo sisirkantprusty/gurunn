2707f24e248371be6e4e67cc725a7672f4877962
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2018-12-29 12:23:26 +0100

    Removed unnecessary test case

--- gurunn/src/test/java/testcases/TC_VerifyTitleAndSortProducts.java
+++ gurunn/src/test/java/testcases/TC_VerifyTitleAndSortProducts.java
@@ -8,26 +8,20 @@ import pageobjects.Guru99HomePage;
 
 public class TC_VerifyTitleAndSortProducts extends TestBase {
 
-	Guru99HomePage guruhome;
+	Guru99HomePage guruHome;
 
 	@Test
 	public void verifyTitleAndSortProducts() throws InterruptedException {
-		guruhome = new Guru99HomePage(dr);
+		guruHome = new Guru99HomePage(dr);
 		System.out.println("this is test");
 		System.out.println(dr.getTitle());
 		Assert.assertTrue(dr.getTitle().equalsIgnoreCase("Home page"),
 				"The page title is not HOME PAGE. Raise a defect");
-		guruhome.clickMobile();
+		guruHome.clickMobile();
 		Thread.sleep(2000);
-		guruhome.selectValueMobileSorting("Name");
-		Assert.assertTrue(guruhome.sortAndComparePhoneNames(), "The sequence of phone names are not in aphabetical order");
+		guruHome.selectValueMobileSorting("Name");
+		Assert.assertTrue(guruHome.sortAndComparePhoneNames(),
+				"The sequence of phone names are not in aphabetical order");
 		System.out.println("verify title is completed");
-	}
-
-	// @Test
-	public void verifyTitle() {
-		guruhome = new Guru99HomePage(dr);
-		guruhome.clickTV();
-
 	}
 }

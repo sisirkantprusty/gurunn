f8bcee79a0a45a4953d2b452d1bd17052367dfaa
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2018-12-30 11:00:41 +0100

    TC to verify that user cant add more than Product available in the store

---
+++ gurunn/src/test/java/testcases/TC_VerifyLimitOfShoppingCart.java
@@ -0,0 +1,31 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.TestBase;
+import pageobjects.Guru99HomePage;
+import pageobjects.Guru99ProductListPage;
+
+public class TC_VerifyLimitOfShoppingCart extends TestBase {
+
+	Guru99HomePage guruHome;
+	Guru99ProductListPage guruProdList;
+
+	@Test (description="Test case to verify that user can not add more than Product available in the store")
+	public void verifyLimitOfShoppingCart() throws InterruptedException {
+		guruHome = new Guru99HomePage(dr);
+		guruProdList = new Guru99ProductListPage(dr);
+		guruHome.clickMobile();
+		// Click on first item on the list
+		guruHome.clickFirstItemOnList();
+		guruProdList.enterQuantity(1000);
+		guruProdList.submitAddtoCart();
+		Thread.sleep(1000);
+		Assert.assertTrue(guruProdList.verifyMessage("The maximum quantity allowed for purchase is 500"),
+				"The message - The maximum quantity allowed for purchase is 500 - does not appear");
+		guruProdList.clickLinkCart();
+		Thread.sleep(500);
+		Assert.assertTrue(guruProdList.verifyEmptyCartMessage("You have no items in your shopping cart."), "Cart is not empty");		
+	}
+}

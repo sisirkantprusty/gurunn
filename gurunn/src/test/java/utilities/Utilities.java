dc74c2b534991abdacfaa8f55b1bb5bdbddd0ea8
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2019-01-26 11:20:35 +0100

    Added sleep

--- gurunn/src/test/java/utilities/Utilities.java
+++ gurunn/src/test/java/utilities/Utilities.java
@@ -9,6 +9,12 @@ public class Utilities extends TestBase {
 	public void goToNewWindow() {
 		for (String handle : dr.getWindowHandles()) {
 			dr.switchTo().window(handle);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }

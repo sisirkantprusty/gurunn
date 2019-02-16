2557f81a92aafd35aa65656ce7b0cc138d83df39
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2019-02-16 11:04:07 +0100

    rearranged code

--- gurunn/src/test/java/base/TestBase.java
+++ gurunn/src/test/java/base/TestBase.java
@@ -15,7 +15,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
@@ -35,12 +34,39 @@ public class TestBase {
 	protected static ExtentTest logger;
 	protected static ExtentHtmlReporter reporter;
 
+	public void setUpChromeBrowser() {
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver\\chromedriver.exe");
+		dr = new ChromeDriver();
+		dr.manage().window().maximize();
+		System.out.println("Chrome driver initialized");
+	}
+
+	public void setUpFirefoxBrowser() {
+		System.setProperty("webdriver.gecko.driver",
+				System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver\\geckodriver.exe");
+		dr = new FirefoxDriver();
+		dr.manage().window().maximize();
+		System.out.println("Firefox driver initialized");
+	}
+
+	public void setUpChromeMobileWebBrowser() {
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver\\chromedriver.exe");
+		Map<String, String> mobileEmulation = new HashMap<String, String>();
+		mobileEmulation.put("deviceName", "iPhone X");
+		Map<String, Object> chromeOptions = new HashMap<String, Object>();
+		chromeOptions.put("mobileEmulation", mobileEmulation);
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+		dr = new ChromeDriver(capabilities);
+	}
+
 	@BeforeSuite(alwaysRun = true)
 	public void setUpForTestSuite() {
 		reporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "\\Reports\\learnauto.html");
 		extent = new ExtentReports();
 		extent.attachReporter(reporter);
-
 		// logger= extent.createTest("SAMPLE TEST REPORT");
 	}
 
@@ -58,53 +84,36 @@ public class TestBase {
 	@BeforeMethod(alwaysRun = true)
 	@Parameters("browser")
 	public void initializeTest(String browser) throws InterruptedException, IOException {
-
 		String testUrl, testHmUrl = null;
 		// Read Properties file
 		inputFile = new FileInputStream(
 				System.getProperty("user.dir") + "\\src\\test\\resources\\configFiles\\config.properties");
 		configFile.load(inputFile);
 		ProjectConstants.filePath = configFile.getProperty("SCREENSHOT_FILEPATH").toString();
-		System.out.println("FILE PATH " + ProjectConstants.filePath);
+		// System.out.println("FILE PATH " + ProjectConstants.filePath);
 
 		testUrl = configFile.getProperty("URL").toString();
 		testHmUrl = configFile.getProperty("HMURL").toString();
 
-		System.out.println("Open the browser: " + "\n");
+		System.out.println("Open the browser: " + browser + "\n");
+
 		if (browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver",
-					System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver\\chromedriver.exe");
-			dr = new ChromeDriver();
-			dr.manage().window().maximize();
-			System.out.println("Chrome driver initialized");
+			setUpChromeBrowser();
 		} else if (browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver",
-					System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver\\geckodriver.exe");
-			dr = new FirefoxDriver();
-			dr.manage().window().maximize();
-			System.out.println("Firefox driver initialized");
+			setUpFirefoxBrowser();
 		} else if (browser.equalsIgnoreCase("mobileWeb")) {
-			System.setProperty("webdriver.chrome.driver",
-					System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver\\chromedriver.exe");
-			Map<String, String> mobileEmulation = new HashMap<String, String>();
-			mobileEmulation.put("deviceName", "iPhone X");
-			Map<String, Object> chromeOptions = new HashMap<String, Object>();
-			chromeOptions.put("mobileEmulation", mobileEmulation);
-			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-			capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-			dr = new ChromeDriver(capabilities);
+			setUpChromeMobileWebBrowser();
 		}
+
 		if (browser.equalsIgnoreCase("mobileWeb"))
 			dr.get(testHmUrl);
 		else
 			dr.get(testUrl);
 		Thread.sleep(3000);
-
 	}
 
 	@AfterMethod(alwaysRun = true)
 	public void tearDownTestMethods(ITestResult result) throws IOException {
-
 		inputFile.close();
 		if (result.getStatus() == ITestResult.SUCCESS) {
 			logger.log(Status.PASS, "Testcase is PASSED");
@@ -113,8 +122,7 @@ public class TestBase {
 			logger.log(Status.FAIL, "Testcase is failed");
 		}
 		dr.close();
-		System.out.println("Driver is closed");
-
+		// System.out.println("Driver is closed");
 	}
 
 }

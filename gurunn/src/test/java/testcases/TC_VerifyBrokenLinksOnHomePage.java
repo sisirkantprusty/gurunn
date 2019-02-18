07fdc195328be3f7db9e1a6b8a3a60fd8ecefec4
SISIRKANTPRUSTY <SISIRKANTPRUSTY@9.145.40.187>
2019-02-18 11:38:33 +0100

    Added softassert

--- gurunn/src/test/java/testcases/TC_VerifyBrokenLinksOnHomePage.java
+++ gurunn/src/test/java/testcases/TC_VerifyBrokenLinksOnHomePage.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import base.TestBase;
 import pageobjects.Guru99HomePage;
@@ -12,8 +13,10 @@ import pageobjects.Guru99HomePage;
 public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 	Guru99HomePage guru99HomePage;
 	String[] linksOnGuru99HomePage;
+	SoftAssert softAssert;
 
 	public void verifyLinkActive(String[] linkUrls) {
+		softAssert = new SoftAssert();
 		try {
 			for (String linkUrl : linkUrls) {
 				URL url = new URL(linkUrl);
@@ -21,12 +24,17 @@ public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 				httpURLConnect.setConnectTimeout(3000);
 				httpURLConnect.connect();
 				if (httpURLConnect.getResponseCode() == 200) {
-					System.out.println(linkUrls + " - " + httpURLConnect.getResponseMessage());
-				}
+					System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage());			}
+
+				softAssert.assertTrue(httpURLConnect.getResponseCode() == 200, linkUrl + "does not 200-OK");
+
 				if (httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
-					System.out.println(linkUrls + " - " + httpURLConnect.getResponseMessage() + " - "
+					System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage() + " - "
 							+ HttpURLConnection.HTTP_NOT_FOUND);
 				}
+
+				softAssert.assertFalse(httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND, linkUrl
+						+ " - " + httpURLConnect.getResponseMessage() + " - " + HttpURLConnection.HTTP_NOT_FOUND);
 			}
 		} catch (Exception e) {
 
@@ -36,8 +44,9 @@ public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 
 	@Test
 	public void verifyBrokenLinksOnHomePage() {
+		logger = extent.createTest("Verify broken links on Guru99 Home Page");
 		guru99HomePage = new Guru99HomePage(dr);
-		linksOnGuru99HomePage=guru99HomePage.getLinksOnHomePage();
+		linksOnGuru99HomePage = guru99HomePage.getLinksOnHomePage();
 		verifyLinkActive(linksOnGuru99HomePage);
 
 	}

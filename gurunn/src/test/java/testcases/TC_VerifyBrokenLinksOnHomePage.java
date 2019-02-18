0a360d856b6115d1b897fbd48f2408305998f279
SISIRKANTPRUSTY <SISIRKANTPRUSTY@9.145.40.187>
2019-02-18 13:19:42 +0100

    Added SoftAssert

--- gurunn/src/test/java/testcases/TC_VerifyBrokenLinksOnHomePage.java
+++ gurunn/src/test/java/testcases/TC_VerifyBrokenLinksOnHomePage.java
@@ -3,7 +3,6 @@ package testcases;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -23,18 +22,17 @@ public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 				HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
 				httpURLConnect.setConnectTimeout(3000);
 				httpURLConnect.connect();
+				softAssert.assertTrue(httpURLConnect.getResponseCode() == 200, linkUrl + "does not 200-OK");
 				if (httpURLConnect.getResponseCode() == 200) {
 					System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage());			}
 
-				softAssert.assertTrue(httpURLConnect.getResponseCode() == 200, linkUrl + "does not 200-OK");
+				softAssert.assertFalse(httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND, linkUrl
+						+ " - " + httpURLConnect.getResponseMessage() + " - " + HttpURLConnection.HTTP_NOT_FOUND);
 
 				if (httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
 					System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage() + " - "
 							+ HttpURLConnection.HTTP_NOT_FOUND);
-				}
-
-				softAssert.assertFalse(httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND, linkUrl
-						+ " - " + httpURLConnect.getResponseMessage() + " - " + HttpURLConnection.HTTP_NOT_FOUND);
+				}				
 			}
 		} catch (Exception e) {
 
@@ -48,6 +46,7 @@ public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 		guru99HomePage = new Guru99HomePage(dr);
 		linksOnGuru99HomePage = guru99HomePage.getLinksOnHomePage();
 		verifyLinkActive(linksOnGuru99HomePage);
+		softAssert.assertAll();
 
 	}
 }

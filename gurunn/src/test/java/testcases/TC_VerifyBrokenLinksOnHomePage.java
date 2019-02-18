d749c64f7ebaf5e35716d8c6bfb1ce7ba279941f
Sisirkant <Sisirkant@LAPTOP-F21NF111>
2019-02-18 21:04:05 +0100

    Added dynamic wait

--- gurunn/src/test/java/testcases/TC_VerifyBrokenLinksOnHomePage.java
+++ gurunn/src/test/java/testcases/TC_VerifyBrokenLinksOnHomePage.java
@@ -7,12 +7,14 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import base.TestBase;
+import commonFunctions.commonFunctions;
 import pageobjects.Guru99HomePage;
 
 public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 	Guru99HomePage guru99HomePage;
 	String[] linksOnGuru99HomePage = null;
 	SoftAssert softAssert;
+	commonFunctions commonFunctions;
 
 	public void verifyLinkActive(String[] linkUrls) {
 		softAssert = new SoftAssert();
@@ -24,7 +26,8 @@ public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 				httpURLConnect.connect();
 				softAssert.assertTrue(httpURLConnect.getResponseCode() == 200, linkUrl + "does not 200-OK");
 				if (httpURLConnect.getResponseCode() == 200) {
-					System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage());			}
+					System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage());
+				}
 
 				softAssert.assertFalse(httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND, linkUrl
 						+ " - " + httpURLConnect.getResponseMessage() + " - " + HttpURLConnection.HTTP_NOT_FOUND);
@@ -32,7 +35,7 @@ public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 				if (httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
 					System.out.println(linkUrl + " - " + httpURLConnect.getResponseMessage() + " - "
 							+ HttpURLConnection.HTTP_NOT_FOUND);
-				}				
+				}
 			}
 		} catch (Exception e) {
 
@@ -43,6 +46,8 @@ public class TC_VerifyBrokenLinksOnHomePage extends TestBase {
 	public void verifyBrokenLinksOnHomePage() {
 		logger = extent.createTest("Verify broken links on Guru99 Home Page");
 		guru99HomePage = new Guru99HomePage(dr);
+		commonFunctions = new commonFunctions();
+		commonFunctions.waitForAnElementToBeVisible(guru99HomePage.webElemementLinkMobile(),20);
 		linksOnGuru99HomePage = guru99HomePage.getLinksOnHomePage();
 		verifyLinkActive(linksOnGuru99HomePage);
 		softAssert.assertAll();
